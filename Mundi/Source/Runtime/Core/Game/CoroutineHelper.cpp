cfc601cf5d447d352efe5b755453cdc84281bc26
YEONGBIN PAK <suplem07@naver.com>
2025-10-31 17:04:02 +0900

    코루틴 헬퍼 만들긴 했는데 점검 및 코드 파악 필요

---
+++ Mundi/Source/Runtime/Core/Game/CoroutineHelper.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CoroutineHelper.h"
+#include "YieldInstruction.h"
+
+FCoroutineHelper::FCoroutineHelper(ULuaScriptComponent* InOwner)
+	: OwnerComponent(InOwner), CurrentInstruction(nullptr)
+{
+}
+
+FCoroutineHelper::~FCoroutineHelper()
+{
+	OwnerComponent = nullptr;	// 참조만 할 뿐이므로, nullptr 명시적 선언
+	delete CurrentInstruction;
+}
+
+void FCoroutineHelper::RunSceduler(float DeltaTime)
+{
+	if (ActiveCoroutine.valid() == false) { return; }
+
+	bool bShouldResume = false;
+
+	if (CurrentInstruction)
+	{
+		if (CurrentInstruction->IsReady(this, DeltaTime))
+		{
+			delete CurrentInstruction;
+			CurrentInstruction = nullptr;
+			bShouldResume = true;
+		}
+		else
+		{
+			bShouldResume = true;
+		}
+	}
+
+	if (bShouldResume)
+	{
+		auto Result = ActiveCoroutine(); 
+		if (Result.get_type() == sol::type::userdata)
+		{
+			// 코루틴이 새로운 명령서를 발행(yield)했는지 확인합니다.
+			sol::object YieldResult = Result;
+			CurrentInstruction = YieldResult.as<FYieldInstruction*>();
+		}
+	}
+}
+
+void FCoroutineHelper::StartCoroutine(sol::function EntryPoint)
+{
+	if (EntryPoint.valid())
+	{
+		ActiveCoroutine = EntryPoint;
+		RunSceduler();
+	}
+}
+
+FYieldInstruction* FCoroutineHelper::CreateWaitForSeconds(float Seconds)
+{
+	return new FWaitForSeconds(Seconds);
+}
---
+++ Mundi/Source/Runtime/Core/Game/CoroutineHelper.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "sol.hpp"
+
+class FYieldInstruction;
+class ULuaScriptComponent; // 루아 시스템과 합치지 않아 존재하지 않지만 테스트를 위해 임시 추가
+
+class FCoroutineHelper
+{
+public:
+	FCoroutineHelper(ULuaScriptComponent* InOwner);
+	~FCoroutineHelper();
+
+	void RunSceduler(float DeltaTime = 0.f);
+	void StartCoroutine(sol::function EntryPoint);
+
+	// Lua에서 C++ YieldInstruction 객체를 생성하기 위한 팩토리 함수
+	FYieldInstruction* CreateWaitForSeconds(float Seconds);
+
+private:
+	ULuaScriptComponent* OwnerComponent{ nullptr };
+	sol::coroutine ActiveCoroutine;
+	FYieldInstruction* CurrentInstruction{ nullptr }; // 현재 코루틴이 대기 중인 명령서
+};
+
